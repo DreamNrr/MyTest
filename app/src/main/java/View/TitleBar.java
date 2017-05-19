b50f1d6cac913c8f76c73027a0d8e6ba5f7b9893
DreamNrr <wzh74371233@163.com>
2017-05-19 18:34:48 +0800

    欢迎页面

---
+++ app/src/main/java/View/TitleBar.java
@@ -0,0 +1,15 @@
+package View;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.LinearLayout;
+
+/**
+ * Created by WZH on 2017/5/19.
+ */
+
+public class TitleBar extends LinearLayout {
+    public TitleBar(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+}
---
+++ app/src/main/java/com/example/wzh/mytest/WelcomeActivity.java
@@ -0,0 +1,42 @@
+package com.example.wzh.mytest;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MotionEvent;
+
+public class WelcomeActivity extends AppCompatActivity {
+    private Handler handler;
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_welcome);
+        handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startMainActivity();
+            }
+        },2000);
+    }
+
+    private void startMainActivity() {
+        Intent intent = new Intent(this,MainActivity.class);
+        startActivity(intent);
+        finish();
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        startMainActivity();
+        return super.onTouchEvent(event);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        handler.removeCallbacksAndMessages(null);
+    }
+}
